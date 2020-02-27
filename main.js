1c9c1bcd1e49e785ea76b35470d3c3eb76b2ba2d
JJ Gray <JJ.Gray@Nology.local>
2020-02-27 15:57:30 +0000

    random shooting complete

--- main.js
+++ main.js
@@ -1,42 +1,55 @@
 class Ship {
-  constructor(name, health, shotDamage) {
+  constructor(name, health, shotDamage, id) {
     this.name = name;
     this.health = health;
     this.shotDamage = shotDamage;
-  }
-
-  getShipCard() {
-    return "<div>" + this.name + "<p>" + "HP: "  + this.health + "</p>" + "</div>";
+    this.id = id;
   }
 
   getShot() {
-    this.health = this.health - this.shotDamage;
-    console.log("HP: " + this.health);
+    return (this.health = this.health - this.shotDamage);
+  }
+
+  getShipCard() {
+    return `<div>${this.name}<p id='${this.id}'>HP: ${this.health}</p></div>`;
   }
 }
 
 let ships = [];
 
 const createShip = () => {
-  ships[0] = new Ship("mothership", 100, 9);
+  ships[0] = new Ship("mothership", 100, 9, `mtr${ships.length}`);
   for (let index = 1; index < 6; index++) {
-    ships[index] = new Ship("defender", 80, 10);
+    ships[index] = new Ship("defender", 80, 10, `def${ships.length}`);
   }
   for (let index = 6; index < 14; index++) {
-    ships[index] = new Ship("striker", 45, 12);
+    ships[index] = new Ship("striker", 45, 12, `str${ships.length}`);
   }
   return ships;
 };
 
 createShip();
 
-const printShip = ship => {
-  document.getElementById(ship.name).innerHTML += ship.getShipCard();
-};
+const printShip = ship => (document.getElementById(ship.name).innerHTML += ship.getShipCard());
 
-const getShipCard = () => {
-  ships.forEach(printShip);
-};
+const getShipCard = () => ships.forEach(printShip);
 
 getShipCard();
 
+const chooseShipToFireAt = () => Math.floor(Math.random() * ships.length);
+
+const shootShip = () => {
+  getShot(ships[chooseShipToFireAt()]);
+  document.getElementById(name);
+};
+
+const getShot = ship => {
+  ship.getShot();
+  getUpdatedShipHealth();
+};
+
+const getUpdatedShipHealth = () => ships.forEach(updateHealth);
+
+const updateHealth = ship => {
+  document.getElementById(ship.id).innerHTML = `HP: ${ship.health}`;
+};
